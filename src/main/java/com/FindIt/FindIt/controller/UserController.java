78b4f88d2fa02bc9af7e9f53bd8d116ef7807ef2
gaeunnnnn <gaeunn02@naver.com>
2024-11-06 23:50:55 +0900

    Feat:회원가입 페이지 호출 api

--- src/main/java/com/FindIt/FindIt/controller/UserController.java
+++ src/main/java/com/FindIt/FindIt/controller/UserController.java
@@ -1,9 +1,6 @@
 package com.FindIt.FindIt.controller;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
