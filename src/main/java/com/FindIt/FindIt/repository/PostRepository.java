fb09eec25663b156279db6333ec65fe95d583ed5
jeongseockhyeon <jeong9260@naver.com>
2024-11-07 13:25:44 +0900

    Chore: mapstruct 의존성 추가

--- src/main/java/com/FindIt/FindIt/repository/PostRepository.java
+++ src/main/java/com/FindIt/FindIt/repository/PostRepository.java
@@ -4,4 +4,5 @@ import com.FindIt.FindIt.entity.PostEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PostRepository extends JpaRepository<PostEntity,Long> {
+
 }
