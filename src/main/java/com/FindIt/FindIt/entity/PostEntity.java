3d5e7f2451a7d76f89573dcb08f4836baac0cdef
jeongseockhyeon <jeong9260@naver.com>
2024-11-07 12:03:51 +0900

    Remove: 중복 파일 삭제

--- src/main/java/com/FindIt/FindIt/entity/PostEntity.java
+++ src/main/java/com/FindIt/FindIt/entity/PostEntity.java
@@ -14,7 +14,7 @@ import lombok.*;
 @Setter
 @Builder
 @Entity
-public class PostEntity extends BaseTimeEntity {
+public class PostEntity extends BaseEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
