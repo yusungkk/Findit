159d9e3bc11493928380eca4e8227f0f3cc2ef09
yookyung <cheshirejyoo@gmail.com>
2024-11-07 14:29:22 +0900

    게시글 상세 조회

--- src/main/java/com/FindIt/FindIt/controller/PostController.java
+++ src/main/java/com/FindIt/FindIt/controller/PostController.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -20,5 +21,11 @@ public class PostController {
     public String postListPage(Model model) {
         model.addAttribute("items",postService.findAll());
         return "postList";
+    }
+
+    @GetMapping("/{id}")
+    public String postDetailPage(@PathVariable Long id, Model model) {
+        model.addAttribute("post", postService.findById(id));
+        return "postDetail";
     }
 }
