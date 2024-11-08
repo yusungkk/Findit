aad01e68b208714bb924d04a4f3e0cdfa78f2b4e
jeongseockhyeon <jeong9260@naver.com>
2024-11-08 15:43:07 +0900

    Feat: 로그인 성공 로그 추가

--- src/main/java/com/FindIt/FindIt/globale/config/SecurityConfig.java
+++ src/main/java/com/FindIt/FindIt/globale/config/SecurityConfig.java
@@ -1,12 +1,15 @@
 package com.FindIt.FindIt.globale.config;
 
+import com.FindIt.FindIt.globale.handler.LoginSuccessHandler;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 
 @Configuration
 @EnableWebSecurity
@@ -19,7 +22,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .csrf((auth) -> auth.disable());
+                .csrf(AbstractHttpConfigurer::disable);
         /*비로그인 사용자는 사이트 접속 시 로그인 페이지와 회원가입 페이지만 사용할 수 있게 설정*/
         http
                 .authorizeHttpRequests((auth) -> auth
@@ -34,6 +37,7 @@ public class SecurityConfig {
                         .loginProcessingUrl("/user/login") //스프링 시큐리티 기본 로그인 요청 url에서 지정 url로 변경
                         .usernameParameter("loginId")  // 기본 인자값이 username로 설정되어 있어 인자값을 loginId로 설정
                         .defaultSuccessUrl("/home", true) // 로그인 성공 시 리다이렉트할 경로
+                        .successHandler(new LoginSuccessHandler())
                         .failureUrl("/user/login?error=true") // 로그인 실패 시 리다이렉트할 경로
                         .permitAll());
         http
@@ -43,5 +47,9 @@ public class SecurityConfig {
 
 
         return http.build();
+    }
+    @Bean
+    public AuthenticationSuccessHandler authenticationSuccessHandler() {
+        return new LoginSuccessHandler();
     }
 }
