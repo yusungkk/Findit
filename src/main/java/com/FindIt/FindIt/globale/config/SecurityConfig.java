cce270bff3e50dc941f6d5b0ae416f29bd145b8e
jeongseockhyeon <jeong9260@naver.com>
2024-11-08 14:57:07 +0900

    Feat: 로그인 post 요청 url 변경 및 로그인 페이지 수정

--- src/main/java/com/FindIt/FindIt/globale/config/SecurityConfig.java
+++ src/main/java/com/FindIt/FindIt/globale/config/SecurityConfig.java
@@ -20,16 +20,19 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .csrf((auth) -> auth.disable());
-
+        /*비로그인 사용자는 사이트 접속 시 로그인 페이지와 회원가입 페이지만 사용할 수 있게 설정*/
         http
                 .authorizeHttpRequests((auth) -> auth
                         .requestMatchers("/user/login","/user/signup").permitAll()
                         .anyRequest().authenticated()
                 );
+
+        /*시큐리티 기본 로그인 폼에서 커스텀 로그인 폼으록 변경하여 사용*/
+        /*비로그인 사용자는 사이트 접속 시 로그인 페이지로 리다이렉트 되도록 설정*/
         http
                 .formLogin((auth) -> auth.loginPage("/user/login")
-                        .loginProcessingUrl("/loginProc")
-                        .usernameParameter("loginId")  // 인자값을 loginId로 설정
+                        .loginProcessingUrl("/user/login") //스프링 시큐리티 기본 로그인 요청 url에서 지정 url로 변경
+                        .usernameParameter("loginId")  // 기본 인자값이 username로 설정되어 있어 인자값을 loginId로 설정
                         .defaultSuccessUrl("/home", true) // 로그인 성공 시 리다이렉트할 경로
                         .failureUrl("/user/login?error=true") // 로그인 실패 시 리다이렉트할 경로
                         .permitAll());
