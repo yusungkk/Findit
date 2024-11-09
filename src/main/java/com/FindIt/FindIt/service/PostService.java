72f4d9290d9ea3bc45c837ea496962e0cdcda770
gaeunnnnn <gaeunn02@naver.com>
2024-11-10 03:35:39 +0900

    Refactor:이미지 경로 반환 로직 ImageService로 분리

--- src/main/java/com/FindIt/FindIt/service/PostService.java
+++ src/main/java/com/FindIt/FindIt/service/PostService.java
@@ -9,20 +9,16 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.util.StringUtils;
-import org.springframework.web.multipart.MultipartFile;
 
-import java.io.File;
-import java.io.IOException;
 import java.util.List;
 import java.util.Optional;
-import java.util.UUID;
 
 @RequiredArgsConstructor
 @Service
 public class PostService {
     private final PostRepository postRepository;
     private final PostImgRepository postImgRepository;
+    private final ImageService imageService;
 
     public List<PostEntity> findAll() {
         return postRepository.findAll();
@@ -44,7 +40,7 @@ public class PostService {
 
         // 이미지가 있을 경우, 이미지 저장
         if (postReqDto.getPostImage() != null && !postReqDto.getPostImage().isEmpty()) {
-            String imagePath = uploadImage("post", postReqDto.getPostImage());
+            String imagePath = imageService.uploadImage("post", postReqDto.getPostImage());
             PostImgEntity postImgEntity = new PostImgEntity();
             postImgEntity.setStorePath(imagePath);
             postImgEntity.setPostEntity(postEntity);
@@ -52,40 +48,5 @@ public class PostService {
             postImgRepository.save(postImgEntity);
         }
     }
-
-    // 이미지 업로드 메서드
-    private String uploadImage(String subPath, MultipartFile image) {
-        String imageDirectoryPath = "C:/webserver_storage/"+ subPath +"/";
-        File imageDirectory = new File(imageDirectoryPath);
-
-        if (!imageDirectory.exists()) {
-            imageDirectory.mkdirs();
-        }
-
-        String extension = "";
-        if (image != null) {
-            extension = StringUtils.getFilenameExtension(image.getOriginalFilename());
-            if (extension != null) {
-            } else {
-                throw new IllegalArgumentException("No file extension");
-            }
-        }
-
-        String savePath = imageDirectoryPath;
-        String saveFilename = UUID.randomUUID() +"."+ extension;
-        String fullPath = savePath + saveFilename;
-        File file = new File(fullPath);
-
-        try {
-            image.transferTo(file);
-        } catch (IOException e) {
-            e.printStackTrace();
-            throw new RuntimeException("Image upload failed: " + e.getMessage());
-        }
-        String dbPath = "/upload/"+ subPath +"/" + saveFilename;
-
-        return dbPath;
-    }
-
 
 }
