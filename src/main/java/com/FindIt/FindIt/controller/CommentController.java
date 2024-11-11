0807ffee7831b384be503a24bb3f5fc75f8708ab
jeongseockhyeon <jeong9260@naver.com>
2024-11-11 21:15:13 +0900

    Feat: 메소드 수정 및 답글 수정 추가

--- src/main/java/com/FindIt/FindIt/controller/CommentController.java
+++ src/main/java/com/FindIt/FindIt/controller/CommentController.java
@@ -22,10 +22,8 @@ public class CommentController {
         /*return ResponseEntity.status(HttpStatus.CREATED).body(createdComment);*/
         return "redirect:/post/" + postId;
     }
-    /*@PatchMapping*/ //html 폼 태그 형식 상 post/get 만 지원
-                     // hidden을 사용하여 method 변경할려했으나 적용이 안되어 추후 다른 방도로 변경
-                     // 일단은 post로 적용
-    @PostMapping("{commentId}")
+
+    @PatchMapping("{commentId}")
     public String updateComment(@PathVariable("postId") Long postId, @PathVariable("commentId") Long commentId, @ModelAttribute CommentDto commentDto) {
         commentService.updateComment(commentId, commentDto);
         return "redirect:/post/" + postId;
