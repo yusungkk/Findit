970619806381656741941952eb8a3ef7db67ea14
jeongseockhyeon <jeong9260@naver.com>
2024-11-13 21:20:50 +0900

    Feat: 주석 추가

--- src/main/java/com/FindIt/FindIt/global/vlidation/ValidFile.java
+++ src/main/java/com/FindIt/FindIt/global/vlidation/ValidFile.java
@@ -8,9 +8,9 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-@Target(value = ElementType.FIELD)
-@Retention(value = RetentionPolicy.RUNTIME)
-@Constraint(validatedBy = ValidFileValidator.class)
+@Target(value = ElementType.FIELD)//필드를 타켓으로 설정
+@Retention(value = RetentionPolicy.RUNTIME)//어노테이션 유지 정책, 런타임 시까지 유지하도록 설정
+@Constraint(validatedBy = ValidFileValidator.class)//유효성 검증 시 사용할 클래스 설정
 public @interface ValidFile {
     String message() default "Invalid File";
     Class<?>[] groups() default {};
